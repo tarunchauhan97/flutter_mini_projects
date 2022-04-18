a0b0fa2c7193c0581b686f944ca16e932c1d9085
tarunchauhan97 <tarun.chauhan@4thpointer.com>
2022-04-18 10:56:22 +0530

    error widget exception box

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,33 @@ import 'package:flutter/material.dart';
 void main() {
   // Set the ErrorWidget's builder before the app is started.
   ErrorWidget.builder = (FlutterErrorDetails details) {
+    // In release builds, show a yellow-on-blue message instead:
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
         body: Center(
-          child: Column(
-            children: [
-              Text(details.summary.toString()),
-              Text(details.silent.toString()),
-              Text(details.exception.toString()),
-              Text(details.exceptionAsString()),
-              Text(details.library!.toUpperCase().toString()),
-            ],
+          child: Container(
+            alignment: Alignment.center,
+            width: 250,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.amber[300],
+              boxShadow: [
+                BoxShadow(color: Colors.green, spreadRadius: 3),
+              ],
+            ),
+            child: Text(
+              ' Error!\n ${details.exception}',
+              style: const TextStyle(color: Colors.red, fontSize: 20),
+              textAlign: TextAlign.center,
+              textDirection: TextDirection.ltr,
+            ),
           ),
         ),
       ),
     );
   };
+
   // Start the app.
   runApp(const MyApp());
 }
@@ -27,27 +37,42 @@ void main() {
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const String _title = 'ErrorWidget Sample';
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    // throw ("Error thrown from initState(), This is Dummy Error Example");
-  }
+  bool throwError = false;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Basic "),
+    if (throwError) {
+      // Since the error widget is only used during a build, in this contrived example,
+      // we purposely throw an exception in a build function.
+      return Builder(
+        builder: (BuildContext context) {
+          throw Exception('oh no, You clicked on Button..! | Error Caught');
+        },
+      );
+    } else {
+      return MaterialApp(
+        title: MyApp._title,
+        home: Scaffold(
+          appBar: AppBar(title: const Text(MyApp._title)),
+          body: Center(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    //by setting throwError to true, above if condition will execute.
+                    throwError = true;
+                  });
+                },
+                child: const Text('Do Not Click me')),
+          ),
         ),
-        body: const Text('Flutter Demo Home Page'),
-      ),
-    );
+      );
+    }
   }
 }
